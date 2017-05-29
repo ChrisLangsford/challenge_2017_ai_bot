1043569505df2c997192fcf3e463f204a0868b28
Chris Langsford <langz020@gmail.com>
2017-05-29 17:21:51 +0200

    "moved hunting/chasing decision into it's own fn"

--- bot.js
+++ bot.js
@@ -49,19 +49,21 @@ function fireOrDoNothing(workingDirectory) {
     var stateFile = require(workingDirectory + '/' + stateFileName);
     console.log(workingDirectory + '/' + stateFile);
     console.log("Round: "+stateFile.Round);
-    var target = assessGrid(stateFile);
+    // var target = acquireTarget(stateFile);
+    // console.log(target);
 
-    var fire = 1;
-    if (stateFile.Round == 1) {
-      var xCoordinate = Math.floor(Math.random() * 10);
-      var yCoordinate = Math.floor(Math.random() * 10);
-    } else {
-        //console.log('Target:'+ JSON.stringify(target));
-        xCoordinate = target.X;
-        yCoordinate = target.Y;
-    }
-
-    var payload = fire + "," + xCoordinate + "," + yCoordinate + "\n";
+    // var fire = 1;
+    // if (stateFile.Round == 1) {
+    //   var xCoordinate = Math.floor(Math.random() * 10);
+    //   var yCoordinate = Math.floor(Math.random() * 10);
+    // } else {
+    //     //console.log('Target:'+ JSON.stringify(target));
+    //     xCoordinate = target.X;
+    //     yCoordinate = target.Y;
+    // }
+    //
+    // var payload = fire + "," + xCoordinate + "," + yCoordinate + "\n";
+    var payload = fire(stateFile, acquireTarget(stateFile));
     fs.writeFile(workingDirectory + '/' + commandFileName, payload, function(err) {
         if(err) {
             return console.log(err);
@@ -70,7 +72,7 @@ function fireOrDoNothing(workingDirectory) {
     });
 }
 
-function assessGrid(state){
+function acquireTarget(state){
   "use strict";
   var grid = state.OpponentMap.Cells;
   grid.forEach((e)=>{e.Probability =0});
@@ -101,6 +103,20 @@ function assessGrid(state){
   grid.sort(function(a, b) {
     return parseFloat(a.Probability) - parseFloat(b.Probability);
   });
-  //console.log("Assessor Grid: "+ JSON.stringify(grid.reverse()));
+  //console.log("Assessor Grid: "+ JSON.stringify(grid.reverse()[0]));
   return grid.reverse()[0];
+}
+
+function fire(state, grid) {
+  //console.log("grid"+JSON.stringify(grid));
+  var xCoordinate;
+  var yCoordinate;
+  if (state.Round == 1 || grid.Probability == 0) {
+    xCoordinate = Math.floor(Math.random() * state.MapDimension);
+    yCoordinate = Math.floor(Math.random() * state.MapDimension);
+  } else {
+    xCoordinate = grid.X;
+    yCoordinate = grid.Y;
+  }
+  return "1" + "," + xCoordinate + "," + yCoordinate + "\n";
 }
