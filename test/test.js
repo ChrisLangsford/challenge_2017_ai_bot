5748e5c3835a38edf6aa62cec8f070a806d57000
Chris Langsford <langz020@gmail.com>
2017-05-29 17:31:34 +0200

    started writing fire tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
 var assert = require('chai').assert;
+var state = require('../state_files/eg_state1.json');
+var grid1 = {"Damaged":false,"Missed":false,"X":0,"Y":0,"Probability":0};
+var grid2 = {"Damaged":false,"Missed":false,"X":1,"Y":1,"Probability":1};
 
-// describe('shoot', () => {
-//   describe('hunting shot', () => {
-//
-//   });
-// });
+describe('firing', () => {
+  describe('random shots stay within the map', () => {
+    
+  });
+  describe('fire will work on round 1', () => {
+
+  });
+  describe('fire will work for 0 probability', () => {
+
+  });
+  describe('fire will work after round 1', () => {
+
+  });
+});
