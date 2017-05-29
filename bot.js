513561a82bbe4ad3ddff4c7ac0d11656c22489b0
Chris Langsford <langz020@gmail.com>
2017-05-29 16:28:57 +0200

    removed extra files to start cleaning

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ function fireOrDoNothing(workingDirectory) {
     var stateFile = require(workingDirectory + '/' + stateFileName);
     console.log(workingDirectory + '/' + stateFile);
     console.log("Round: "+stateFile.Round);
-    var target = require('./assessGrid.js')(stateFile);
+    var target = assessGrid(stateFile);
 
     var fire = 1;
     if (stateFile.Round == 1) {
@@ -68,4 +68,39 @@ function fireOrDoNothing(workingDirectory) {
         }
         console.log("A shot was fired");
     });
+}
+
+function assessGrid(state){
+  "use strict";
+  var grid = state.OpponentMap.Cells;
+  grid.forEach((e)=>{e.Probability =0});
+  //console.log(JSON.stringify(grid));
+  var cell = {};
+  for (var i = 0; i < grid.length; i++) {
+
+    if (grid[i].Damaged) {
+        grid[i].Probability = -1;
+      if (grid[i+1] && !(grid[i+1].Damaged || grid[i+1].Missed)) {
+        grid[i+1].Probability += 1;
+      }
+      if (grid[i-1] && !(grid[i-1].Damaged || grid[i-1].Missed)) {
+        grid[i-1].Probability += 1;
+      }
+      if (grid[i+10] && !(grid[i+10].Damaged || grid[i+10].Missed)) {
+        grid[i+10].Probability += 1;
+      }
+      if (grid[i-10] && !(grid[i-10].Damaged || grid[i-10].Missed)) {
+        grid[i-10].Probability += 1;
+      }
+    }
+
+    if (grid[i].Missed) {
+      grid[i].Probability = -1;
+    }
+  }
+  grid.sort(function(a, b) {
+    return parseFloat(a.Probability) - parseFloat(b.Probability);
+  });
+  //console.log("Assessor Grid: "+ JSON.stringify(grid.reverse()));
+  return grid.reverse()[0];
 }
