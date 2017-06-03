abcbbd29a3e4d1d709239a23e475dfff5c031d23
Chris Langsford <langz020@gmail.com>
2017-06-03 08:34:46 +0200

    "added chasing algorithm"

--- selectFiringMethod.js
+++ selectFiringMethod.js
@@ -2,10 +2,14 @@ module.exports = function selectFiringMethod(state) {
 var targetCell;
 var battleMap = require('./mapReader.js')(state);
 //hunt or chase
-do {
-  targetCell = require('./huntingShot.js')(state.MapDimension);
-  targetOnMap = battleMap.get(targetCell.X, targetCell.Y);
-} while (targetOnMap.Damaged || targetOnMap.Missed);
+
+targetCell = require('./chasingShot.js')(state);
+if (!(targetCell.Probability && targetCell.Probability > 0)) {
+  do {
+    targetCell = require('./huntingShot.js')(state.MapDimension);
+    targetOnMap = battleMap.get(targetCell.X, targetCell.Y);
+  } while (targetOnMap.Damaged || targetOnMap.Missed);
+};
 
 return targetCell;
 };
