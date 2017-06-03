53763523be43dbcea25616c73997eb4119c15c3e
Chris Langsford <langz020@gmail.com>
2017-06-03 07:51:50 +0200

    "Added parity check on random shot"

--- huntingShot.js
+++ huntingShot.js
@@ -1,9 +1,12 @@
 module.exports = function huntingShot(dim) {
   var targetCell = {
     "X": 0,
-    "Y":0
+    "Y": 0
   };
-  targetCell.X = Math.floor(Math.random() * dim);
-  targetCell.Y = Math.floor(Math.random() * dim);
+
+  do {
+    targetCell.X = Math.floor(Math.random() * dim);
+    targetCell.Y = Math.floor(Math.random() * dim);
+  } while (!((targetCell.X % 2 == 0 && targetCell.Y % 2 ==0) || (targetCell.X % 2 == 1 && targetCell.Y % 2 == 1)));
 return targetCell;
 };
