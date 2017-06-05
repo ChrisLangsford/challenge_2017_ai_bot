f157ae3d60b930905bcfc3617697a93764ff7289
Chris Langsford <langz020@gmail.com>
2017-06-05 17:53:21 +0200

    "fixed weird wrapping issue"

--- getProbabilitySpread.js
+++ getProbabilitySpread.js
@@ -7,12 +7,14 @@ battleMap.forEach((e=>{grid.push(e)}));
 var cell = {};
   for (var i = 0; i < grid.length; i++) {
 
+
+
     if (grid[i].Damaged) {
         grid[i].Probability = -1;
       if (grid[i+1] && !(grid[i+1].Damaged || grid[i+1].Missed)) {
         grid[i+1].Probability += 1;
       }
-      if (grid[i-1] && !(grid[i-1].Damaged || grid[i-1].Missed)) {
+      if (grid[i-1] && grid[i-1].Y !=(dim-1) && !(grid[i-1].Damaged || grid[i-1].Missed)) {
         grid[i-1].Probability += 1;
       }
       if (grid[i+dim] && !(grid[i+dim].Damaged || grid[i+dim].Missed)) {
@@ -25,8 +27,16 @@ var cell = {};
     if (grid[i].Missed) {
       grid[i].Probability = -1;
     }
+
+    // if (grid[i].X == 2 && grid[i].Y == 1) {
+    //   console.log("i: "+ JSON.stringify(grid[i]));
+    //   console.log("+1: "+ JSON.stringify(grid[i+1]));
+    //   console.log("-1: "+ JSON.stringify(grid[i-1]));
+    //   console.log("+dim: "+ JSON.stringify(grid[i+dim]));
+    //   console.log("-dim: "+ JSON.stringify(grid[i-dim]));
+    // }
   }
-//console.log(grid);
+console.log(grid);
 
 return grid;
 };
