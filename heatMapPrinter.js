e1f3a27353b82075ca019a43c799eb14a3daddcc
Chris Langsford <langz020@gmail.com>
2017-07-15 08:40:23 +0200

    "reoriented heatmap, still upside down"

--- heatMapPrinter.js
+++ heatMapPrinter.js
@@ -2,15 +2,24 @@ module.exports = function heatmapPrinter(state) {
   var row = "";
   var map = require('./potentialShipFinder.js')(state);
   for (var i = 0; i < state.MapDimension; i++) {
-    for (var j = 0; j < map.getRow(i).length; j++) {
-      var prob = " "+ String(map.get(i,j).Probability)
-      if (prob.length <=2) {
-        prob = " "+prob;
+    for (var j = 0; j < state.MapDimension; j++) {
+
+      var cell = map.get(j,i);
+      if (cell.Damaged) {
+        row+=' * ';
+      } else if (cell.Missed) {
+        row+=' ! ';
       }
-      row += prob;
+       else {
+        var prob = " " + String(map.get(j,i).Probability)
+
+        if (prob.length <= 2) {
+          prob = " "+prob;
+        }
+        row += prob;
+      }
     }
     row +='\r\n';
-    //console.log(row);
   }
 
   return row;
