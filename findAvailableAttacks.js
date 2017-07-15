019007f5fc87257f217fdcec2eb5a0ad553e0018
Chris Langsford <langz020@gmail.com>
2017-07-15 17:00:21 +0200

    "added function to return available powers"

---
+++ findAvailableAttacks.js
@@ -0,0 +1,16 @@
+module.exports = function (state) {
+  var energyAvailable = state.PlayerMap.Owner.Energy;
+  //1. which attacks are available to use?
+  //2. which of those attacks require <= energy than available
+  var powers = [];
+  state.PlayerMap.Owner.Ships.forEach((ship)=>{
+    if (ship.Placed && !ship.Destroyed) {
+      ship.Weapons.forEach((weapon)=>{
+        if (weapon.WeaponType != 'SingleShot' && weapon.EnergyRequired <= energyAvailable) {
+          powers.push(weapon);
+        }
+      });
+    }
+   });
+   return powers;
+}
