19e91ca1c94782b4aa9b9292d039817c327a1729
Chris Langsford <langz020@gmail.com>
2017-07-30 10:51:56 +0200

    "fixed parity check on weapons"

--- powers_module/cornerShotCells.js
+++ powers_module/cornerShotCells.js
@@ -4,11 +4,11 @@ module.exports = function crossShotCellsMaker(state) {
 
   battleMap.forEach((cell)=>{
     if(battleMap.getNeighbours(cell.X, cell.Y).length == 8){
-      if (battleMap.get(cell.X, cell.Y).Damaged || battleMap.get(cell.X, cell.Y).Missed) {
+      if (battleMap.get(cell.X, cell.Y).Damaged || battleMap.get(cell.X, cell.Y).Missed && battleMap.get(cell.X, cell.Y).Checkered) {
             var possibleCornerShot = true;
             battleMap.getNeighbours(cell.X, cell.Y).forEach((e)=>{
               var counter = 0;
-              if (counter == 1 || counter == 3 || counter == 4 || counter == 6) {
+              if (possibleCornerShot && (counter == 1 || counter == 3 || counter == 4 || counter == 6)) {
                 if (e.Damaged ||e.Missed) {
                   possibleCornerShot = false;
                 }
